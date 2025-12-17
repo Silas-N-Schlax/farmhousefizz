5dada70cdf3f845346eb4e483a0e93bd24f77a56
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-12-17 16:17:47 -0500

    Added netlify config, and fixed jsx syntax error causing build failure.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import './App.css'
 import { HashRouter as Router , Routes, Route} from 'react-router-dom'
 import { Home } from './pages/home'
 import { NotFound } from './pages/notfound'
-import { SimpleAnalytics } from '@simpleanalytics/react'
+import { SimpleAnalytics } from "@simpleanalytics/react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/*" element={<NotFound/>} />
-      </Routes>
-    </Router>
-    <SimpleAnalytics />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/*" element={<NotFound/>} />
+        </Routes>
+      </Router>
+      <SimpleAnalytics />
+    </>
   )
 }
 
