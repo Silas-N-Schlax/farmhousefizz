4728726881dcd243cd2fbe90f634f0772de610ad
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-12-17 16:12:07 -0500

    Added simple analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { HashRouter as Router , Routes, Route} from 'react-router-dom'
 import { Home } from './pages/home'
 import { NotFound } from './pages/notfound'
+import { SimpleAnalytics } from '@simpleanalytics/react'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/*" element={<NotFound/>} />
       </Routes>
     </Router>
+    <SimpleAnalytics />
   )
 }
 
