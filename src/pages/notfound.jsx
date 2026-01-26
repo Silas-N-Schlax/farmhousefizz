f1076d5e4f50179ee2c716061cc499962125d064
SilasNS <107065641+silasSN@users.noreply.github.com>
2026-01-26 13:21:11 -0500

    Added a little card on 404 page with note and link back to home page.

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -8,7 +8,13 @@ export function NotFound() {
         <title>404 - Page Not Found</title>
         <meta name="description" content="Page Not Found" />
       </Helmet>
-      <h1>404 - Page Not Found</h1>
+      <div className="notFound-card-container">
+        <div className="notFound-card">
+          <h1>404: Page Not Found</h1>
+          <p>Oops. This page fizzled out. Head back to Farmhouse Fizz to find our menu, events, and handcrafted dirty sodas.</p>
+          <a style={{textDecoration: "none", color: "var(--molten-lava)"}} href="/">Home</a>
+        </div>
+      </div>
     </>
   )
 }
