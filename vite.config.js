e99a2f5a3ce501db89ca9be1c4a1f6694dcb077f
SilasNS <107065641+silasSN@users.noreply.github.com>
2026-01-26 13:32:27 -0500

    Added sitemap rendering for SEO and GSC to crawl and index site faster.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      sitemap({
+      hostname: "https://farmhousefizz.com",
+      dynamicRoutes: [
+        "/",
+        "/about-us",
+        "/contact-us",
+        "/menu",
+        "/polls/soda-poll",
+        "/questions-and-answers",
+      ],
+      exclude: [
+        "/terms-and-conditions", 
+        "/privacy-policy"
+      ],
+    }),
+    ],
 });
