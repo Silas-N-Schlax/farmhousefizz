9656990ef078a272f8ea6557b655c49bdd300f0c
SilasNS <107065641+silasSN@users.noreply.github.com>
2026-05-08 20:30:52 -0400

    Add new route for catering page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from './pages/home'
 import { NotFound } from './pages/notfound'
 import { AboutUs } from './pages/aboutUs'
 import { ContactUs } from './pages/contactUs'
+import { Catering } from './pages/catering'
 import { Menu } from './pages/menu'
 import { Privacy } from './pages/privacy'
 import { QuestionsAndAnswers } from './pages/questionsAndAnswers'
@@ -25,6 +26,7 @@ function App() {
               <Route path="/*" element={<NotFound/>} />
               <Route path="/about-us" element={<AboutUs/>} />
               <Route path="/contact-us" element={<ContactUs/>} />
+              <Route path="/catering" element={<Catering/>} />
               <Route path="/menu" element={<Menu/>} />
               <Route path="/questions-and-answers" element={<QuestionsAndAnswers/>} />
               <Route path="/polls/soda-poll" element={<SodaPoll/>} />
