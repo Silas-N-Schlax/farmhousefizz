686f5c01fa21348de0169241d75ed1d0bd5200a7
SilasNS <107065641+silasSN@users.noreply.github.com>
2026-05-12 15:34:30 -0400

    Update sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,37 @@
+const DOMAIN = "https://farmhousefizz.com"
+const CURRENT_DATE = new Date().toISOString()
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
+import Sitemap from 'vite-plugin-sitemap'
+import { cateringLinks } from './src/assets/seo/catering-links';
+const cateringURLs = cateringLinks()
+
+let cateringRoutes = cateringURLs.map(url => url.path)
+
+let routes = [
+  "/",
+  "/about-us",
+  "/contact-us",
+  "/catering",
+  "/menu",
+  "/questions-and-answers",
+]
+
+let dynamicRoutes = [...routes, ...cateringRoutes]
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
       react(),
-      sitemap({
-      hostname: "https://farmhousefizz.com",
-      dynamicRoutes: [
-        "/",
-        "/about-us",
-        "/contact-us",
-        "/catering",
-        "/menu",
-        "/polls/soda-poll",
-        "/questions-and-answers",
-        "/energy-drink-notice"
-      ],
+      Sitemap({
+      hostname: DOMAIN,
+      dynamicRoutes: dynamicRoutes,
       exclude: [
         "/terms-and-conditions", 
-        "/privacy-policy"
-      ],
+        "/polls/soda-poll",
+        "/privacy-policy",
+        "/energy-drink-notice"
+      ]
     }),
     ],
 });
