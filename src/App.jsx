5e3bc4b02d8af3262ac924cf9bdadb00279aa15a
SilasNS <silasschlax@gmail.com>
2026-09-01 08:14:08 -0400

    refactor(styles): rename styleKit to stylesheet, adopt optics css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './styleKit/styles.css'
+import './stylesheet/styles.css'
 import { BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 import { Home } from './pages/home'
 import { NotFound } from './pages/notfound'
